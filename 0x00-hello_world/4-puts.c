a62717012aac896a4aab0dcc114f5a953fe182d0
Lister Miselo <talislister1@gmail.com>
2022-09-12 11:53:49 -0700

    Variables

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * main - prints exactly "Programming is like building a multilingual puzzle",
- * followed by a new line.
- * Returns: Always 0.
+ *main - Prints "Programming is like building a multilingual
+ * puzzle", followed by a new line.
+ *
+ * Return: Always 0.
  */
 
 int main(void)
 
 {
-	puts("\"Programming is like building a multigual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
