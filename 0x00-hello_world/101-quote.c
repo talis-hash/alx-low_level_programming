2fd0ddf54b4b751c9369031959a18fb7b68b6449
Lister Miselo <talislister1@gmail.com>
2022-09-12 12:08:09 -0700

    Variables

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
- * main - prints exactly "and that piece of art is useful "
- *  - Dora Korpar, 2015-10-19, followed by a new line,
- *  to the standard error.
+ * main - Prints "and that piece of art is useful" -
+ * Dora Korpar, 2015-10-19,followed
+ * by a new line, to standard error.
  *
- * Return: Always 1 (Success)
+ * Return: Always 1.
+ *
  */
 
 int main(void)
-
 {
-	write(STDOUT_FILENO, "and that piece of art is useful\"- Dora Korpar,
-			2015-10-19\n", 59);
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		59);
 
-			return (1);
+	return (1);
+
 }
