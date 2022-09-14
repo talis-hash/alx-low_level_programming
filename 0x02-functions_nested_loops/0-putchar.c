7c7d4d0b2bf095c2225aecb5eb42f1cc4a144845
Lister Miselo <talislister1@gmail.com>
2022-09-14 10:27:44 -0700

    Nested while loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-nclude "_putchar.h"
-
+#include "main.h"
 /**
- * main - Prints "_putchar" followed by a new line.
- *
- * Return: Always 0.
- *     */
+* main - Prints "_putchar" followed by a new line.
+*
+* Return: Always 0.
+*/
 
 int main(void)
-
 {
-       	_putchar('_');
+	_putchar('_');
 	_putchar('p');
-      	_putchar('u');
-       	_putchar('t');
-        _putchar('c');
- 	_putchar('h');
-       	_putchar('a');
-       	_putchar('r');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
 	return (0);
 }
