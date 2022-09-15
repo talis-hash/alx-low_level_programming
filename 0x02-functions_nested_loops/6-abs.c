5d5c9d88bad206f4c26db7b7e490fad228f870cf
Lister Miselo <talislister1@gmail.com>
2022-09-14 19:59:20 -0700

    Nested while loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,11 +1,12 @@
-nclude "main.h"
+#include "main.h"
 /**
-* _abs - computes the absolute value of an integer
-* @n: the int to check
-*Return: the absolute value of int
-*/
+ *_abs - computes the absolute value of an integer
+ *@n: the int to check
+ *Return: the absolute value of int
+ */
 
 int _abs(int n)
+
 {
 	if (n >= 0)
 	{
