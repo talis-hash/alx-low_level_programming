d555aabf0a604ff9f5d569a56a62ba90971562e5
Lister Miselo <talislister1@gmail.com>
2022-09-15 15:06:16 -0700

    fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,12 +2,11 @@
 #include "main.h"
 
 /**
-*print_number - Prints a number
-* @n: The number to print
-*/
+ * print_number - Prints a number
+ * @n: The number to print
+ */
 
 void print_number(int n)
-
 {
 	unsigned int num = n;
 
