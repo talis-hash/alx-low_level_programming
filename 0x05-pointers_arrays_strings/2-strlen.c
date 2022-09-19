314c048eae38038cd971bb01000a7f2e93601b13
Lister Miselo <talislister1@gmail.com>
2022-09-19 14:48:48 -0700

    Array

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *strlen - returns the length of a string
+ *_strlen - returns the length of a string
  * @s: string to evaluate
+ *
  * Return: the length of the string
  */
-
 int _strlen(char *s)
 
 {
