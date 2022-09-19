5e123de0bd65e59e928038bdcaed11112996c37e
Lister Miselo <talislister1@gmail.com>
2022-09-19 07:42:46 -0700

    Arrays

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+nclude "main.h"
+
+/**
+ *reset_to_98 - updates the value of the variable
+ *the pointer points to to 98
+ *@n: pointer to the variable to update
+ */
+
+void reset_to_98(int *n)
+
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Reademe file for 0x05-pointers_arrays_strings
