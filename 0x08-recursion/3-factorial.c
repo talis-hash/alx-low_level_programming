9e2cbbbce49aa5d4df848bf8b42734339f86696c
Lister Miselo <talislister1@gmail.com>
2022-09-28 14:34:13 -0700

    Quick fix

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,26 +1,22 @@
 #include "main.h"
 
 /**
-* _pow_recursion - returns the value of x raised to the power of y.
-* @x: base.
-* @y: exponent.
-* Return: value of the exponentiation.
-*/
-
-int _pow_recursion(int x, int y)
+ * factorial - returns the factorial of a number
+ * @n: number to return the factorial from
+ * Return: factorial of n
+ */
+int factorial(int n)
 
 {
 
-	if (y < 0)
+	if (n < 0)
 
 		return (-1);
 
-	else if (y == 0)
+	if (n == 0)
 
 		return (1);
 
-	else
-
-		return (x * _pow_recursion(x, y - 1));
+	return (n * factorial(n - 1));
 
 }
--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,56 +1,24 @@
 #include "main.h"
 
 /**
-* power_operation - returns the natural square root of a number.
-* @n: input number.
-* @c: iterator.
-* Return: square root or -1.
-*/
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: value to raise
+ * @y: power
+ * Return: result of the power
+ */
 
-int power_operation(int n, int c)
-
-{
-
-	if (c % (n / c) == 0)
-
-	{
-
-		if (c * (n / c) == n)
-
-			return (c);
-
-		else
-
-			return (-1);
-
-	}
-
-	return (0 + power_operation(n, c + 1));
-
-}
-
-/**
-*   _sqrt_recursion - returns the natural square root of a number.
-* @n: input number.
-* Return: natural square root.
-*/
-
-int _sqrt_recursion(int n)
+int _pow_recursion(int x, int y)
 
 {
 
-	if (n < 0)
+	if (y < 0)
 
 		return (-1);
 
-	if (n == 0)
-
-		return (0);
-
-	if (n == 1)
+	if (y == 0)
 
 		return (1);
 
-	return (power_operation(n, 2));
+	return (x * _pow_recursion(x, y - 1));
 
 }
