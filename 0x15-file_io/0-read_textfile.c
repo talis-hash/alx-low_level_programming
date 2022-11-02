46f3468d290d03be21f0510b68f79679271120e7
Lister Miselo <talislister1@gmail.com>
2022-11-01 19:16:30 -0700

    fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,31 +10,30 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 
 {
-    ssize_t file, fread, fwrite;
-    char *totalSize;
+	ssize_t file, fread, fwrite;
+	char *totalSize;
 
-    totalSize = malloc(sizeof(char) * letters);
-    if (totalSize == NULL)
+	totalSize = malloc(sizeof(char) * letters);
+       	if (totalSize == NULL)
+	       	return (0);
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	       	return (0);
+
+	fread = read(file, totalSize, letters);
+       	if (fread == -1)
 	    return (0);
 
-    if (filename == NULL)
-
+	fwrite = write(STDOUT_FILENO, totalSize, fread);
+	if (fwrite == -1)
 	    return (0);
 
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-	    return (0);
-
-    fread = read(file, totalSize, letters);
-    if (fread == -1)
-	    return (0);
-
-    fwrite = write(STDOUT_FILENO, totalSize, fread);
-    if (fwrite == -1)
-	    return (0);
-
-    close(file);6
-    free(totalSize);
-    return (fwrite);
+       	close(file);
+	free(totalSize);
+	return (fwrite);
 
 }
